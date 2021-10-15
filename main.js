0f65637d377a0208215da372dbacaf956ad19333
kutumin <kutumin@gmail.com>
2021-10-15 12:38:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const img = document.getElementById("img");
-
-const url = 'https://picsum.photos/103/102'
-
 const btn = document.querySelector('.button');
 
 btn.addEventListener('click', async() => {
-    let response = await fetch(url);
-    let blob = await response.blob();
-    let img = document.createElement('img');
-    document.body.append(img);
-    img.src = URL.createObjectURL(blob);
+    var inpObj = document.getElementById('input-data');
+    var inpObj1 = document.getElementById('input-limit');
+    if (!inpObj.checkValidity()) {
+        demo.innerHTML = 'Неправильно значение страницы \r' + inpObj.validationMessage; }
+    else if  (!inpObj1.checkValidity()) {
+        demo.innerHTML = 'Неправильно значение лимита \r' + inpObj1.validationMessage; }
+    else{
+      var url = "https://picsum.photos/" + inpObj.value + '/' + inpObj1.value;
+      console.log(url);
+      let response = await fetch(url);
+      let blob = await response.blob();
+      let img = document.createElement('img');
+      document.body.append(img);
+      img.src = URL.createObjectURL(blob);}
   });
